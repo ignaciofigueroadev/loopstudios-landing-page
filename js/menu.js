f85d24861432603a8684d6ae0a20ff1541bb6157
Figueroa <figueroaignaciodev@gmail.com>
2023-04-05 17:39:19 -0300

    Finishing de mobile page

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,26 @@
 const openMenu = document.querySelector(".header__menu--open");
 const closeMenu = document.querySelector(".header__menu--close");
 const nav = document.querySelector(".header__nav");
+const navItems = document.querySelectorAll(".header__item");
+const heroTitle = document.querySelector(".header__title");
 
 const showMenu = () => {
   nav.classList.add("header__nav--active");
   closeMenu.style.display = "block";
   openMenu.style.display = "none";
+  // heroTitle.style.zIndex = "-5";
 };
 
 const removeMenu = () => {
   nav.classList.remove("header__nav--active");
   closeMenu.style.display = "none";
   openMenu.style.display = "block";
+  // heroTitle.style.zIndex = "1";
 };
 
 openMenu.addEventListener("click", showMenu);
 closeMenu.addEventListener("click", removeMenu);
+
+navItems.forEach((item) => {
+  item.addEventListener("click", removeMenu);
+});
